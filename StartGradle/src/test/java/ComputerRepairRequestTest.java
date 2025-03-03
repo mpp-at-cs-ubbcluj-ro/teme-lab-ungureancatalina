236dde3792cc1fea8fa339c059fcfb2ac9c3ad2c
Ungurean Cătălina <ungurean.catalina2005@gmail.com>
2025-03-03 17:05:36 +0200

    gata

---
+++ StartGradle/src/test/java/ComputerRepairRequestTest.java
@@ -0,0 +1,24 @@
+import com.google.common.annotations.VisibleForTesting;
+import model.ComputerRepairRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ComputerRepairRequestTest {
+    @Test
+    @DisplayName("Fisrt Test")
+    public void testExample(){
+        ComputerRepairRequest crr=new ComputerRepairRequest();
+        assertEquals("",crr.getOwnerName());
+        assertEquals("",crr.getOwnerAddress());
+    }
+
+    @Test
+    @DisplayName("Test Exemplu")
+    public void testExample2(){
+        assertEquals(2,2,"Numerele ar trebui sa fie egale");
+    }
+      
+}
